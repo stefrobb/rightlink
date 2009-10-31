34fedb33eea71b209e7b39410adedf6ae679c305
Leonid Borisenko <leo.borisenko@gmail.com>
2009-10-31 19:15:21 +0200

    Handle links that have been added to document dynamically

--- content_script/rightlink_extension.js
+++ content_script/rightlink_extension.js
@@ -1,7 +1,7 @@
 var port_to_controller = chrome.extension.connect();
 function openUrlInNewBackgroundTab(url) { port_to_controller.postMessage(url) }
 
-$('a').bind('contextmenu', function(event) {
+$('a').live('contextmenu', function(event) {
   if (event.altKey || event.ctrlKey) return;
 
   event.preventDefault();
