18acceb66e278126bf6db4e684158f711760d9b0
Leonid Borisenko <leo.borisenko@gmail.com>
2009-12-20 21:55:45 +0200

    Bind right click event handler only once for page (i.e. Document object).
    This fix prevents creation of several identical background tabs after right
    clicking on any link at http://eBay.com.

--- content_script/rightlink_extension.js
+++ content_script/rightlink_extension.js
@@ -1,10 +1,15 @@
 var port_to_controller = chrome.extension.connect();
 function openUrlInNewBackgroundTab(url) { port_to_controller.postMessage(url) }
 
-$('a').live('contextmenu', function(event) {
+function handleRightClickOnLink(event)
+{
   if (event.altKey || event.ctrlKey) return;
 
   event.preventDefault();
   openUrlInNewBackgroundTab(this.href);
-});
+}
+
+var flag = 'Chrome_RightLink_extension_was_already_injected_in_this_document';
+if (!document[flag]) $('a').live('contextmenu', handleRightClickOnLink);
+document[flag] = true;
 
